53c19cff11c508c67e7021de6cb14700a17c93f3
pawlenko <tiprosquad@gmail.com>
2018-03-09 15:09:32 +0100

    View changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
 
     def index
-        redirect_to new_user_session_url
+        if !user_signed_in?
+            redirect_to new_user_session_url
+         else 
+            
+         end
     end
 end
